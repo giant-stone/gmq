9ddb9732207023582ebed2490f9fb019d7db7b3a
Tony Wong <63030915+tonytony2020@users.noreply.github.com>
2022-06-27 18:29:20 +0800

    add NewServer test

---
+++ gmq/gmq_test.go
@@ -0,0 +1,37 @@
+package gmq_test
+
+import (
+	"context"
+	"flag"
+	"testing"
+
+	"github.com/giant-stone/gmq/gmq"
+	"github.com/giant-stone/go/glogging"
+	"github.com/go-redis/redis/v8"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	dsnRedis string
+)
+
+func init() {
+	glogging.Init([]string{"stdout"}, "debug")
+
+	flag.StringVar(&dsnRedis, "dsnRedis", "redis://localhost:6379/14", "redis data source name for testing")
+}
+
+func setup(tb testing.TB) (broker gmq.Broker) {
+	tb.Helper()
+
+	opts, err := redis.ParseURL(dsnRedis)
+	require.NoError(tb, err, "redis.ParseURL")
+
+	cli := redis.NewClient(opts)
+	err = cli.FlushDB(context.Background()).Err()
+	require.NoError(tb, err, "cli.FlushDB")
+
+	broker, err = gmq.NewBrokerFromRedisClient(cli)
+	require.NoError(tb, err, "gmq.NewBrokerFromRedisClient")
+	return broker
+}
