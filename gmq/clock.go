dc1bedc5bc10b02d82c74d7678e27b1a0f4770b3
Tony Wong <63030915+tonytony2020@users.noreply.github.com>
2022-06-28 11:12:49 +0800

    add SimulatedClock and test

--- gmq/clock.go
+++ gmq/clock.go
@@ -1,6 +1,9 @@
 package gmq
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 type Clock interface {
 	Now() time.Time
@@ -14,4 +17,33 @@ type WallClock struct{}
 
 func (it *WallClock) Now() time.Time {
 	return time.Now()
+}
+
+// A SimulatedClock is a Clock implementation that doesn't "tick" on its own.
+// Change time by explicit call the AdvanceTime() or SetTime() functions.
+type SimulatedClock struct {
+	mu sync.Mutex
+	t  time.Time
+}
+
+func NewSimulatedClock(t time.Time) *SimulatedClock {
+	return &SimulatedClock{t: t}
+}
+
+func (it *SimulatedClock) Now() time.Time {
+	it.mu.Lock()
+	defer it.mu.Unlock()
+	return it.t
+}
+
+func (it *SimulatedClock) SetTime(t time.Time) {
+	it.mu.Lock()
+	defer it.mu.Unlock()
+	it.t = t
+}
+
+func (it *SimulatedClock) AdvanceTime(d time.Duration) {
+	it.mu.Lock()
+	defer it.mu.Unlock()
+	it.t = it.t.Add(d)
 }
